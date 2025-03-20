371af4f3cb75d45d3659f5561c5e48c85c28fa61
Ben Whittle <benwhittle31@gmail.com>
2025-03-20 14:04:09 -0500

    clean up comments

--- src/mcp/tools/select/types.ts
+++ src/mcp/tools/select/types.ts
@@ -1,20 +1,16 @@
 import { z } from 'zod';
 import { DatabaseRecord, DatabaseRecordSchema } from '../types';
 
-// Name -----------------
-
 export const SELECT_TOOL_NAME = 'select';
-
-// Input -----------------
 
 export type SelectToolInput = {
   query: string;
 };
+
 export const SelectToolInputSchema = z.object({
   query: z.string(),
 });
 
-// Output -----------------
-
 export type SelectToolOutput = DatabaseRecord[];
+
 export const SelectToolOutputSchema = z.array(DatabaseRecordSchema);
